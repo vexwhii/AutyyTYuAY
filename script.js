a5fd5055ea025dadac3bb373ebdeece33a99dc0c
vexwhii <85463425+vexwhii@users.noreply.github.com>
2023-08-05 14:57:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function copyToClipboard() {
+    const numberElement = document.getElementById('number');
+    const numberText = numberElement.textContent || numberElement.innerText;
 
+    if (navigator.clipboard && window.isSecureContext) {
+        navigator.clipboard.writeText(numberText)
+            .then(() => {
+                alert('Number copied to clipboard!');
+            })
+            .catch((err) => {
+                console.error('Failed to copy to clipboard: ', err);
+            });
+    } else {
+        const textarea = document.createElement('textarea');
+        textarea.value = numberText;
+        textarea.style.position = 'fixed'; // Prevent scrolling to bottom of page on mobile
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            document.execCommand('copy');
+            alert('Number copied to clipboard!');
+        } catch (err) {
+            console.error('Failed to copy to clipboard: ', err);
+        } finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
